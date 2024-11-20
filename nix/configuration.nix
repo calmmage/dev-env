1353bbc109cc810ff1312539c868eb7340b9ef7d
Reliable Magician <petr.b.lavrov@gmail.com>
2024-11-20 22:34:36 +0300

    Small fixes - enable vscode

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -19,54 +19,54 @@
       nodePackages.npm
       nodePackages.pnpm
     ];
-    # programs.vscode = {
-    #   enable = true;
-    #   enableUpdateCheck = false;
-    #   enableExtensionUpdateCheck = false;
-    #   userSettings = {
-    #     "aws.telemetry" = false;
-    #     "sqltools.dependencyManager" = {
-    #       "packageManager" = "npm";
-    #       "installArgs" = [ "install" ];
-    #       "runScriptArgs" = [ "run" ];
-    #       "autoAccept" = false;
-    #     };
-    #     "sqltools.useNodeRuntime" = true;
-    #     "window.zoomLevel" = 1;
-    #     "redhat.telemetry.enabled" = false;
-    #     "markdown-pdf.executablePath" = "/Applications/Google Chrome.app";
-    #     "files.associations" = { "*.py" = "python"; };
-    #     "[typescript]" = { };
-    #     "[nix]" = { "editor.defaultFormatter" = "brettm12345.nixfmt-vscode"; };
-    #     "[python]" = {
-    #       "editor.formatOnType" = true;
-    #       "editor.defaultFormatter" = "charliermarsh.ruff";
-    #     };
-    #     "github.copilot.editor.enableAutoCompletions" = false;
-    #     "dev.containers.dockerPath" = "/etc/profiles/per-user/petr/bin/docker";
-    #     "direnv.path.executable" = "/etc/profiles/per-user/petr/bin/direnv";
-    #     "css.enabledLanguages" = "nunjucks html";
-    #     "amazonQ" = {
-    #       "shareContentWithAWS" = false;
-    #       "telemetry" = false;
-    #     };
-    #     "continue.enableTabAutocomplete" = false;
-    #   };
-    #   extensions = with pkgs.vscode-extensions; [
-    #     ms-vscode.cpptools-extension-pack
-    #     mkhl.direnv
-    #     bbenoist.nix
-    #     brettm12345.nixfmt-vscode
-    #     ms-python.python
-    #     ms-python.debugpy
-    #     charliermarsh.ruff
-    #     ms-toolsai.jupyter
-    #     ms-vscode-remote.remote-containers
-    #     ecmel.vscode-html-css
-    #     redhat.vscode-yaml
-    #     foxundermoon.shell-format
-    #   ];
-    # };
+     programs.vscode = {
+       enable = true;
+       enableUpdateCheck = false;
+       enableExtensionUpdateCheck = false;
+       userSettings = {
+         "aws.telemetry" = false;
+         "sqltools.dependencyManager" = {
+           "packageManager" = "npm";
+           "installArgs" = [ "install" ];
+           "runScriptArgs" = [ "run" ];
+           "autoAccept" = false;
+         };
+         "sqltools.useNodeRuntime" = true;
+         "window.zoomLevel" = 1;
+         "redhat.telemetry.enabled" = false;
+         "markdown-pdf.executablePath" = "/Applications/Google Chrome.app";
+         "files.associations" = { "*.py" = "python"; };
+         "[typescript]" = { };
+         "[nix]" = { "editor.defaultFormatter" = "brettm12345.nixfmt-vscode"; };
+         "[python]" = {
+           "editor.formatOnType" = true;
+           "editor.defaultFormatter" = "charliermarsh.ruff";
+         };
+         "github.copilot.editor.enableAutoCompletions" = false;
+         "dev.containers.dockerPath" = "/etc/profiles/per-user/petr/bin/docker";
+         "direnv.path.executable" = "/etc/profiles/per-user/petr/bin/direnv";
+         "css.enabledLanguages" = "nunjucks html";
+         "amazonQ" = {
+           "shareContentWithAWS" = false;
+           "telemetry" = false;
+         };
+         "continue.enableTabAutocomplete" = false;
+       };
+       extensions = with pkgs.vscode-extensions; [
+         ms-vscode.cpptools-extension-pack
+         mkhl.direnv
+         bbenoist.nix
+         brettm12345.nixfmt-vscode
+         ms-python.python
+         ms-python.debugpy
+         charliermarsh.ruff
+         ms-toolsai.jupyter
+         ms-vscode-remote.remote-containers
+         ecmel.vscode-html-css
+         redhat.vscode-yaml
+         foxundermoon.shell-format
+       ];
+     };
 
     programs.git.extraConfig = {
       user.email = lib.mkForce userConfig.email;
