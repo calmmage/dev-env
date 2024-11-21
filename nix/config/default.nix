a405706a4a047301ea7470eedf11ded71ab4947a
Reliable Magician <petrlavrov@calmmage.com>
2024-11-21 18:47:55 +0300

    Do a proper dict merge for configs

--- nix/config/default.nix
+++ nix/config/default.nix
@@ -3,11 +3,31 @@ let
   shared = import ./shared.nix { inherit pkgs; };
   petrWork = import ./petr-work.nix { inherit pkgs; };
   petrPersonal = import ./petr-personal.nix { inherit pkgs; };
+  
+  # Helper function for deep merging
+  recursiveMerge = attr1: attr2:
+    let
+      f = attrPath:
+        let
+          set1 = builtins.getAttr attrPath attr1;
+          set2 = builtins.getAttr attrPath attr2;
+        in
+        if builtins.isAttrs set1 && builtins.isAttrs set2
+        then set1 // set2
+        else if builtins.isList set1 && builtins.isList set2
+        then set1 ++ set2
+        else set2;
+    in
+    builtins.zipAttrsWith (name: values: f name) [ attr1 attr2 ];
 in
 {
   userconfigs = {
-    petr = shared // petrWork;
-    petrlavrov = shared // petrPersonal;
+    # use this for shallow merge - to OVERRIDE shared configs with personal
+    #    petr = shared // petrWork;
+    #    petrlavrov = shared // petrPersonal;
+    # use this for deep merge - to merge the internal list and dicts
+    petr = recursiveMerge shared petrWork;
+    petrlavrov = recursiveMerge shared petrPersonal;
     default = shared;
   };
 }
