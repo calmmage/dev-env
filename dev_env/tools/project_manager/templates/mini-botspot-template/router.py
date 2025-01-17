27d0a755c07d5fec46c00a3e9301af90489b1d68
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-01-17 20:20:33 +0300

    v0.2.33: Update mini-botspot-template
    add required versions of botspot and calmlib
    - update pyproject.toml and lock file
    
    use send_safe and command menu botspot utils

--- dev_env/tools/project_manager/templates/mini-botspot-template/router.py
+++ dev_env/tools/project_manager/templates/mini-botspot-template/router.py
@@ -1,18 +1,23 @@
-from _app import App
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
+from botspot.components.bot_commands_menu import add_command
+from botspot.utils import send_safe
+
+from _app import App
 
 router = Router()
 app = App()
 
 
+@add_command("start", "Start the bot")
 @router.message(CommandStart())
 async def start_handler(message: Message):
-    await message.answer(f"Hello! Welcome to {app.name}!")
+    await send_safe(message.chat.id, f"Hello! Welcome to {app.name}!")
 
 
+@add_command("help", "Show this help message")
 @router.message(Command("help"))
 async def help_handler(message: Message):
     """Basic help command handler"""
-    await message.answer(f"This is {app.name}. Use /start to begin.")
+    await send_safe(message.chat.id, f"This is {app.name}. Use /start to begin.")
