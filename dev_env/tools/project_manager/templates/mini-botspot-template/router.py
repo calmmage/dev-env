eee1b19367d18962c01ae0f49fdc7c41b1510f64
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-03-06 23:58:10 +0300

    Update nix setup

--- dev_env/tools/project_manager/templates/mini-botspot-template/router.py
+++ dev_env/tools/project_manager/templates/mini-botspot-template/router.py
@@ -2,20 +2,20 @@ from _app import App
 from aiogram import Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
-from botspot.components import bot_commands_menu
+from botspot import commands_menu
 from botspot.utils import send_safe
 
 router = Router()
 app = App()
 
 
-@bot_commands_menu.add_command("start", "Start the bot")
+@commands_menu.add_command("start", "Start the bot")
 @router.message(CommandStart())
 async def start_handler(message: Message):
     await send_safe(message.chat.id, f"Hello! Welcome to {app.name}!")
 
 
-@bot_commands_menu.add_command("help", "Show this help message")
+@commands_menu.add_command("help", "Show this help message")
 @router.message(Command("help"))
 async def help_handler(message: Message):
     """Basic help command handler"""
