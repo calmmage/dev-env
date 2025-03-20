1978f3a3aeadbceaf43544c599e3b02403736bb8
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-03-20 03:43:23 +0300

    Add mini-botspot-template dev notebook for bot

--- dev_env/tools/project_manager/templates/mini-botspot-template/router.py
+++ dev_env/tools/project_manager/templates/mini-botspot-template/router.py
@@ -9,13 +9,13 @@ router = Router()
 app = App()
 
 
-@commands_menu.add_command("start", "Start the bot")
+@commands_menu.botspot_command("start", "Start the bot")
 @router.message(CommandStart())
 async def start_handler(message: Message):
     await send_safe(message.chat.id, f"Hello! Welcome to {app.name}!")
 
 
-@commands_menu.add_command("help", "Show this help message")
+@commands_menu.botspot_command("help", "Show this help message")
 @router.message(Command("help"))
 async def help_handler(message: Message):
     """Basic help command handler"""
