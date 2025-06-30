da7c34d1806f4447b911c713b5d71b2e3f9af793
Petr Lavrov <petr.b.lavrov@gmail.com>
2025-06-30 14:55:54 +0300

    Add new aliases

--- nix/modules/shell/aliases/new.nix
+++ nix/modules/shell/aliases/new.nix
@@ -4,5 +4,9 @@
   programs.zsh.shellAliases = {
     # Poetry commands
     bump = "poetry version patch";  # Increment patch version number
+    bump-minor = "poetry version minor";  # Increment minor version number
+    bump-major = "poetry version major";  # Increment major version number
+
+    uvup = "uv sync --upgrade --group test --group dev --group extras --group docs";
   };
 } 
